178eb79ebc161be5c136ec9d5fe0fb9a819feb16
Bruce Lopez <blopez6@uncc.edu>
2018-03-22 18:10:05 -0400

    auth works

--- client/components/accounts.js
+++ client/components/accounts.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
 
 class Accounts extends Component {
   componentDidMount() {
-    // Render the Blaze accounts form then find the div
-    // we just rendered in the 'render' method and place
-    // the Blaze accounts form in that div
+    this.view = Blaze.render(Template.loginButtons,
+      this.container
+    );
   }
 
   componentWillUnmount() {
-    // Go find the forms we created and destroy them
-    // We need to clean up those forms ourselves
+    Blaze.remove(this.view);
   }
 
   render() {
     return (
-      <div></div>
+      <div ref={node => {
+        const container = this.container = node;
+        return container;
+        }}
+      />
     );
   }
 }
